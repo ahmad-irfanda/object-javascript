02d9a2f5d1511bf4967f8e01ee4f2771d905571e
ahmad-irfanda <selalumoveon@gmail.com>
2019-09-18 22:15:33 +0700

    Create facebook-exercise.js
    we create simple objects and arrays (data structures) in javascript.
    
    https://www.instagram.com/ahmad_irfanda_

---
+++ facebook-exercise.js
@@ -0,0 +1,32 @@
+// Create an object and an array which we will use in our facebook exercise. 
+// author : https://www.instagram.com/ahmad_irfanda_
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+{
+    username: "irfanda",
+    password: "supersecret"
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [
+    {
+        username: "irfanda", 
+        password: "supersecret"
+    }
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+    {
+        username: "John", 
+        timeline: "So tired from all that learning!"
+    },
+    {
+        username: "Winston", 
+        timeline: "Javascript is sooo cool!"
+    },
+    {
+        username: "Mike", 
+        timeline: "Javascript is pretty cool!"
+    }
+];
